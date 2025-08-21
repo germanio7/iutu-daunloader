93f0a8b96e1f787909ebd91365f3a0b16badca71
German Escobar <german.e.escobar@gmail.com>
2025-08-20 22:06:39 -0300

    Actualizar host de la aplicación a '0.0.0.0' para permitir conexiones externas en modo desarrollo.

--- app.py
+++ app.py
@@ -105,4 +105,4 @@ if __name__ == '__main__':
     print(f'Aplicación ejecutándose en: {app_url}')
     print('Modo desarrollo')
     
-    app.run(debug=True, port=port, host='127.0.0.1')
+    app.run(debug=True, port=port, host='0.0.0.0')
